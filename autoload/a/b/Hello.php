b8b12248d7d40f775304f1ebb1f65391c1f9b399
Jefferson Lyu <jefferson.lyu@gmail.com>
2013-06-22 11:13:44 +0800

    a simple example for autoloading

---
+++ autoload/a/b/Hello.php
@@ -0,0 +1,8 @@
+<?php
+namespace a\b;
+
+class Hello {
+    function __construct($a) {
+        echo $a, PHP_EOL;
+    }
+}
---
+++ autoload/hello.php
@@ -0,0 +1,6 @@
+<?php
+require_once "autoloader.php";
+use a\b\Hello;
+
+$h = new Hello(123);
+$h = new Hello("海涛涛");
